2383e6d321ac610c6facc3d8ebd4fa3eecf452f3
smarapati-devops <stalin.marapati.devops@gmail.com>
2023-08-29 20:11:41 +0530

    project added

---
+++ sampleapp/sampleapp/sampleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleappApp.swift
+//  sampleapp
+//
+//  Created by Capgemini on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct sampleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
